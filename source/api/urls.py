085ecc443ed351d8fa4826701cdbcaaedf2e9978
Artur <metis130383@gmail.com>
2020-10-24 02:11:23 +0600

    Добавлени ViewSet  для  цитат и  потключен в  urls

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import QuoteViewSet
+
+
+router = DefaultRouter()
+router.register('quote', QuoteViewSet, basename='quote')
+
 
 app_name = 'api'
 
-urlpatterns = [
 
+urlpatterns = [
+    path('', include(router.urls))
 ]
